d9c97e1ebfa3ca6f6eab69ea083a9305d6e34e92
neresr0d <rodrigoneres030@gmail.com>
2024-09-12 21:33:08 -0300

    configurações retornadas

--- configs/config.php
+++ configs/config.php
@@ -1,9 +1,8 @@
-
 <?php
-define('NOME_BANCO', 'if0_37252279_estante_web');
-define('LOCAL_BANCO', 'sql305.infinityfree.com');
-define('USUARIO', 'if0_37252279');
-define('SENHA', 'r0dr1g02024');
+define('NOME_BANCO', 'estante_web');
+define('LOCAL_BANCO', 'localhost');
+define('USUARIO', 'root');
+define('SENHA', '');
 
 
 
